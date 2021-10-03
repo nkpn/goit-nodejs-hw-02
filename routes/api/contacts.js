9d3d7999a28de2592e00023949683de216ce1ee6
nkpn <illinmikita@gmail.com>
2021-10-03 21:24:08 +0300

    updated import

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contacts = require('../../model/contacts.json');
+const Contacts = require('../../model/index.js');
 
 router.get('/', async (req, res, next) => {
   try {
